d16f5de9da0b6ef2ad846d3889f6938b9354b7b2
stevenflin <steven.lin@stern.nyu.edu>
2018-08-23 17:05:57 -0400

    enable cors

--- server.js
+++ server.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 
 const port = process.env.PORT || 8080;
 
+app.use((req, res, next) => {
+	res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.use('/api/users', usersController);
 app.use('/api/pools', poolsController);
 
