f93f9ceae455de4cfe55b235c0f0d09037ed35b1
Juan Diego Donneys <33184416+JuanDiegoDC@users.noreply.github.com>
2018-08-28 13:45:02 -0700

    passport, cookie-session, mongodb user registration routes
    * passport, cookie-session, mongodb user registration routes
    
    * fixes pull request, loginauth backend

---
+++ routes/auth-routes.js
@@ -0,0 +1,88 @@
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const User = require('../models/user.js');
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+// registering new user
+router.post('/register', (req, res) => {
+  let { name, email, password, passwordconfirm } = req.body;
+  if (password === passwordconfirm) {
+    if (password && name && email) {
+      bcrypt.hash(password, saltRounds).then(function(hash){
+
+        console.log('Hash is:', hash);
+
+        email = email.toLowerCase();
+
+        User.find({ email: email }, (err, previousUser) => {
+          console.log('Previous User is: ', previousUser);
+          if (err) {
+            res.json({
+              error: 'Server error.'
+            });
+          }
+          else if (previousUser.length > 0) {
+            res.json({
+              error: 'Account already exists.'
+            });
+            res.end();
+          }
+          else {
+            const newUser = new User ({
+              name: name,
+              email: email,
+              password: hash
+            });
+            newUser.save().then((user) => {
+              if (!user) {
+                res.status(500).json({
+                  error: 'Failed to save user.'
+                });
+              }
+              else {
+                res.status(200).json({
+                  success: true
+                });
+              }
+            })
+            .catch((err) => {
+              console.log('Error: ', err);
+            })
+          }
+        })
+      })
+      .catch((err) => {
+        console.log('Error: ', err);
+      });
+    }
+  }
+  else {
+    res.json({
+      error: 'Passwords must match.'
+    });
+  }
+});
+
+// auth login
+router.get('/login', (req, res) => {
+  res.send('Logging In!');
+});
+
+// auth logout
+router.get('/logout', (req, res) => {
+  res.send('Logging Out!');
+});
+
+// auth with google
+router.get('/google', passport.authenticate('google', {
+  scope: ['profile']
+}));
+
+// auth callback for google
+router.get('/google/redirect', passport.authenticate('google'), (req, res) => {
+  res.send('Success, you have verified your account with Google.')
+});
+
+module.exports = router;
---
+++ routes/passport-setup.js
@@ -0,0 +1,42 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+const User = require('../models/user');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById((id, done) => {
+    done(null, user);
+  })
+});
+
+passport.use(
+  new GoogleStrategy({
+    clientID: process.env.clientID,
+    clientSecret: process.env.clientSecret,
+    callbackURL: "/api/auth/google/redirect",
+  }, (accessToken, refreshToken, profile, done) => {
+    // passport callback function
+    User.findOne({ googleId: profile.id })
+    .then((currentUser) => {
+      if (currentUser) {
+        console.log('The current user is:', currentUser);
+        done(null, currentUser);
+      }
+      else {
+        new User({
+          name: profile.name,
+          email: profile.email,
+          googleId: profile.id
+        })
+        .save()
+        .then((newUser) => {
+          console.log('New User Created: ', newUser);
+          done(null, newUser);
+        })
+      }
+    })
+  })
+)
